6517a499ef269606d4231c05970b6439adc42eac
Wendell Soares <wendell.soares@gmail.com>
2023-12-21 18:12:16 -0300

    filtro e pesquisa

--- script.js
+++ script.js
@@ -4,7 +4,136 @@ const API_URL = BASE_URL + '/discover/movie?language=pt-BRsort_by=popularity.des
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const searchURL = BASE_URL + '/search/movie?'+API_KEY;
 
+const genres = [
+    {
+      "id": 28,
+      "name": "Action"
+    },
+    {
+      "id": 12,
+      "name": "Adventure"
+    },
+    {
+      "id": 16,
+      "name": "Animation"
+    },
+    {
+      "id": 35,
+      "name": "Comedy"
+    },
+    {
+      "id": 80,
+      "name": "Crime"
+    },
+    {
+      "id": 99,
+      "name": "Documentary"
+    },
+    {
+      "id": 18,
+      "name": "Drama"
+    },
+    {
+      "id": 10751,
+      "name": "Family"
+    },
+    {
+      "id": 14,
+      "name": "Fantasy"
+    },
+    {
+      "id": 36,
+      "name": "History"
+    },
+    {
+      "id": 27,
+      "name": "Horror"
+    },
+    {
+      "id": 10402,
+      "name": "Music"
+    },
+    {
+      "id": 9648,
+      "name": "Mystery"
+    },
+    {
+      "id": 10749,
+      "name": "Romance"
+    },
+    {
+      "id": 878,
+      "name": "Science Fiction"
+    },
+    {
+      "id": 10770,
+      "name": "TV Movie"
+    },
+    {
+      "id": 53,
+      "name": "Thriller"
+    },
+    {
+      "id": 10752,
+      "name": "War"
+    },
+    {
+      "id": 37,
+      "name": "Western"
+    }
+  ]
+
 const main = document.getElementById('main');
+const form =  document.getElementById('form');
+const search = document.getElementById('pesquisa');
+const tagsEl = document.getElementById('tags');
+
+
+var selectedGenre = []
+setGenre();
+
+function setGenre() {
+    tagsEl.innerHTML= '';
+    genres.forEach(genre => {
+        const t = document.createElement('div');
+        t.classList.add('tag');
+        t.id=genre.id;
+        t.innerText = genre.name;
+        t.addEventListener('click', () => {
+            if(selectedGenre.length == 0){
+                selectedGenre.push(genre.id);
+            } else{
+                if(selectedGenre.includes(genre.id)){
+                    selectedGenre.forEach((id, idx) => {
+                        if(id == genre.id){
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                }else{
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getMovies(API_URL + '&with_genres='+encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(t);
+    })
+}
+
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag =>{
+        tag.classList.remove('highlight')
+    })
+    if(selectedGenre.length != 0){
+        selectedGenre.forEach(id => {
+            const highlightedTag = document.getElementById(id)
+            highlightedTag.classList.add('highlight')
+
+        })
+    }
+}
 
 getMovies(API_URL);
 
@@ -12,7 +141,11 @@ function getMovies(url) {
 
     fetch(url).then(res => res.json()).then(data =>{
         console.log(data.results)
-        showMovies(data.results);
+        if(data.results.length !==0){
+            showMovies(data.results);
+        } else {
+            main.innerHTML= `<h1 class="resultado">Nenhum filme encontrado</h1>`
+        }
 
     })
 }
@@ -51,3 +184,15 @@ function getColor(vote) {
         return 'vermelho'
     }
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = pesquisa.value;
+    if(searchTerm) {
+        getMovies(searchURL+'&query='+searchTerm)
+    }else{
+        getMovies(API_URL);
+    }
+
+})
